1cb620c5aa9c2a5cba6fcfb779733c9b2f5bac1e
Megan Zehrt <zehrtmegan@gmail.com>
2025-04-23 16:07:29 -0500

    Contains Duplicate Algo

---
+++ Contains_Duplicate.Algo.23.April/algo.js
@@ -0,0 +1,34 @@
+// 217. Contains Duplicate
+
+
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+
+ - Given an integer array 'nums', return true if any value appears at least twice in the array
+ - return false otherwise
+
+ visted = new set
+ iterate through array
+    if set has number
+        return true
+
+ return false
+ */
+ var containsDuplicate = function (nums) {
+
+    let visited = new Set()
+
+    for (let num of nums) {
+        if (visited.has(num)) {
+            return true
+        }
+
+        visited.add(num)
+    }
+
+    return false
+};
