6bf6633f5233bcaddbf2f97b3d0c8c6c51185055
Megan Zehrt <zehrtmegan@gmail.com>
2025-04-23 17:09:55 -0500

    Valid Anagram - Second Solution

--- Valid_Anagram.Algo.23.April/algo.js
+++ Valid_Anagram.Algo.23.April/algo.js
@@ -26,3 +26,35 @@ var isAnagram = function(s, t) {
     return false
     
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    let map = new Map()
+
+    if(s.length !== t.length) return false
+
+    for(let letter of s){
+
+        map.set(letter, (map.get(letter) || 0) + 1)
+    }
+
+    for(let letter of t){
+
+        if(map.has(letter)){
+            map.set(letter, (map.get(letter) - 1))
+
+            if(map.get(letter) == 0){
+                map.delete(letter)
+            }
+        } else{
+            return false
+        }
+    }
+    
+    return map.size == 0 ? true : false
+};
