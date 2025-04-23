f93548f23d9b014c43c1f79cfda72d0199d2908c
Megan Zehrt <zehrtmegan@gmail.com>
2025-04-23 16:21:44 -0500

    Valid Anagram

--- Contains_Duplicate.Algo.23.April/algo.js
+++ Contains_Duplicate.Algo.23.April/algo.js
@@ -4,6 +4,9 @@
 
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+
+
+
 /**
  * @param {number[]} nums
  * @return {boolean}
---
+++ Valid_Anagram.Algo.23.April/algo.js
@@ -0,0 +1,28 @@
+// Valid Anagram
+
+
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    if(s.length != t.length) return false
+
+    let sortedS = s.split('').sort().join('')
+    let sortedT = t.split('').sort().join('')
+
+    if(sortedS == sortedT){
+        return true
+    }
+
+    return false
+    
+};
